3efb000bec2b0fd8f9824850df5d8e6b07d81fd6
jatin sardana <jatinsardana2000@gmail.com>
2024-11-10 01:52:11 +0530

    a

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
   return (
     <>
     <h1 className='text-4xl font-extrabold flex justify-center items-center text-center mb-6'>
-      Meme Generator
+      Meme Generator - Jatin Sardana
     </h1>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4">
       {data && data.map((e) => (
