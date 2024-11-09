550a2b614d997bd02f4cd05eab761fa98d39233f
jatin sardana <jatinsardana2000@gmail.com>
2024-11-10 01:46:00 +0530

    deleted

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React, { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Papp from "./Papp.jsx";
 import { BrowserRouter } from "react-router-dom";
 import Mapp from "./Components/Mapp.jsx";
 
